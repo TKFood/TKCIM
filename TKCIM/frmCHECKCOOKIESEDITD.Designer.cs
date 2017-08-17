579a69d74b71a2f9253db23c92c116c72a981d17
at3158 <tk290@tkfood.com.tw>
2017-08-17 09:50:14 +0800

    UPDATECHECKCOOKIESMD

--- TKCIM/frmCHECKCOOKIESEDITD.Designer.cs
+++ TKCIM/frmCHECKCOOKIESEDITD.Designer.cs
@@ -45,6 +45,20 @@
             this.label10 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.textBox901 = new System.Windows.Forms.TextBox();
+            this.textBox801 = new System.Windows.Forms.TextBox();
+            this.textBox701 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
@@ -88,7 +102,6 @@
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 100F));
             this.tableLayoutPanel1.Controls.Add(this.label4, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.textBox101, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.textBox601, 1, 5);
             this.tableLayoutPanel1.Controls.Add(this.textBox501, 1, 4);
             this.tableLayoutPanel1.Controls.Add(this.textBox201, 1, 1);
             this.tableLayoutPanel1.Controls.Add(this.textBox401, 1, 3);
@@ -97,8 +110,23 @@
             this.tableLayoutPanel1.Controls.Add(this.label3, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.label8, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.label7, 0, 3);
-            this.tableLayoutPanel1.Controls.Add(this.label10, 0, 4);
-            this.tableLayoutPanel1.Controls.Add(this.label11, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 12);
+            this.tableLayoutPanel1.Controls.Add(this.label13, 0, 11);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 0, 10);
+            this.tableLayoutPanel1.Controls.Add(this.label12, 0, 9);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 0, 8);
+            this.tableLayoutPanel1.Controls.Add(this.label9, 0, 7);
+            this.tableLayoutPanel1.Controls.Add(this.label11, 0, 6);
+            this.tableLayoutPanel1.Controls.Add(this.label10, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.textBox601, 1, 6);
+            this.tableLayoutPanel1.Controls.Add(this.textBox701, 1, 7);
+            this.tableLayoutPanel1.Controls.Add(this.textBox801, 1, 8);
+            this.tableLayoutPanel1.Controls.Add(this.textBox901, 1, 9);
+            this.tableLayoutPanel1.Controls.Add(this.dateTimePicker3, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox1, 1, 10);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox3, 1, 11);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox4, 1, 12);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -147,7 +175,7 @@
             // textBox601
             // 
             this.textBox601.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox601.Location = new System.Drawing.Point(103, 206);
+            this.textBox601.Location = new System.Drawing.Point(103, 246);
             this.textBox601.Name = "textBox601";
             this.textBox601.Size = new System.Drawing.Size(147, 27);
             this.textBox601.TabIndex = 111;
@@ -157,6 +185,7 @@
             this.textBox501.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox501.Location = new System.Drawing.Point(103, 166);
             this.textBox501.Name = "textBox501";
+            this.textBox501.ReadOnly = true;
             this.textBox501.Size = new System.Drawing.Size(147, 27);
             this.textBox501.TabIndex = 110;
             // 
@@ -174,6 +203,7 @@
             this.textBox401.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox401.Location = new System.Drawing.Point(103, 126);
             this.textBox401.Name = "textBox401";
+            this.textBox401.ReadOnly = true;
             this.textBox401.Size = new System.Drawing.Size(147, 27);
             this.textBox401.TabIndex = 117;
             // 
@@ -182,6 +212,7 @@
             this.textBox301.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox301.Location = new System.Drawing.Point(103, 86);
             this.textBox301.Name = "textBox301";
+            this.textBox301.ReadOnly = true;
             this.textBox301.Size = new System.Drawing.Size(147, 27);
             this.textBox301.TabIndex = 116;
             // 
@@ -212,44 +243,44 @@
             this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label8.Location = new System.Drawing.Point(3, 84);
+            this.label8.Location = new System.Drawing.Point(3, 92);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(94, 32);
+            this.label8.Size = new System.Drawing.Size(94, 16);
             this.label8.TabIndex = 108;
-            this.label8.Text = "破損餅乾(kg)";
+            this.label8.Text = "單別";
             // 
             // label7
             // 
             this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label7.Location = new System.Drawing.Point(3, 124);
+            this.label7.Location = new System.Drawing.Point(3, 132);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(94, 32);
+            this.label7.Size = new System.Drawing.Size(94, 16);
             this.label7.TabIndex = 107;
-            this.label7.Text = "落地餅乾(kg)";
+            this.label7.Text = "單號";
             // 
             // label10
             // 
             this.label10.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label10.Location = new System.Drawing.Point(3, 172);
+            this.label10.Location = new System.Drawing.Point(3, 212);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(94, 16);
             this.label10.TabIndex = 118;
-            this.label10.Text = "餅乾屑(kg)";
+            this.label10.Text = "時間";
             // 
             // label11
             // 
             this.label11.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label11.Location = new System.Drawing.Point(3, 212);
+            this.label11.Location = new System.Drawing.Point(3, 252);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(94, 16);
             this.label11.TabIndex = 120;
-            this.label11.Text = "報廢袋數";
+            this.label11.Text = "重量";
             // 
             // tabPage2
             // 
@@ -261,6 +292,149 @@
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(3, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 16);
+            this.label1.TabIndex = 121;
+            this.label1.Text = "品名";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(3, 492);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 16);
+            this.label2.TabIndex = 122;
+            this.label2.Text = "主管";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(3, 412);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 16);
+            this.label5.TabIndex = 123;
+            this.label5.Text = "檢查結果";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(3, 332);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 16);
+            this.label6.TabIndex = 124;
+            this.label6.Text = "溫度";
+            // 
+            // label9
+            // 
+            this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label9.Location = new System.Drawing.Point(3, 292);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(94, 16);
+            this.label9.TabIndex = 125;
+            this.label9.Text = "長度";
+            // 
+            // label12
+            // 
+            this.label12.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label12.Location = new System.Drawing.Point(3, 372);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(94, 16);
+            this.label12.TabIndex = 126;
+            this.label12.Text = "溼度";
+            // 
+            // label13
+            // 
+            this.label13.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label13.Location = new System.Drawing.Point(3, 452);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(94, 16);
+            this.label13.TabIndex = 127;
+            this.label13.Text = "填表人";
+            // 
+            // textBox901
+            // 
+            this.textBox901.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox901.Location = new System.Drawing.Point(103, 366);
+            this.textBox901.Name = "textBox901";
+            this.textBox901.Size = new System.Drawing.Size(147, 27);
+            this.textBox901.TabIndex = 128;
+            // 
+            // textBox801
+            // 
+            this.textBox801.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox801.Location = new System.Drawing.Point(103, 326);
+            this.textBox801.Name = "textBox801";
+            this.textBox801.Size = new System.Drawing.Size(147, 27);
+            this.textBox801.TabIndex = 129;
+            // 
+            // textBox701
+            // 
+            this.textBox701.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox701.Location = new System.Drawing.Point(103, 286);
+            this.textBox701.Name = "textBox701";
+            this.textBox701.Size = new System.Drawing.Size(147, 27);
+            this.textBox701.TabIndex = 130;
+            // 
+            // dateTimePicker3
+            // 
+            this.dateTimePicker3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.dateTimePicker3.Checked = false;
+            this.dateTimePicker3.CustomFormat = "HH:mm";
+            this.dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker3.Location = new System.Drawing.Point(103, 206);
+            this.dateTimePicker3.Name = "dateTimePicker3";
+            this.dateTimePicker3.Size = new System.Drawing.Size(147, 27);
+            this.dateTimePicker3.TabIndex = 131;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "OK",
+            "NG"});
+            this.comboBox1.Location = new System.Drawing.Point(103, 408);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(147, 24);
+            this.comboBox1.TabIndex = 132;
+            this.comboBox1.Text = "OK";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(103, 448);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(147, 24);
+            this.comboBox3.TabIndex = 133;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(103, 488);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(147, 24);
+            this.comboBox4.TabIndex = 134;
+            // 
             // frmCHECKCOOKIESEDITD
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
@@ -268,7 +442,7 @@
             this.ClientSize = new System.Drawing.Size(984, 739);
             this.Controls.Add(this.tabControl1);
             this.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmCHECKCOOKIESEDITD";
             this.Text = "frmCHECKCOOKIESEDITD";
             this.tabControl1.ResumeLayout(false);
@@ -298,5 +472,19 @@
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox701;
+        private System.Windows.Forms.TextBox textBox801;
+        private System.Windows.Forms.TextBox textBox901;
+        private System.Windows.Forms.DateTimePicker dateTimePicker3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
     }
 }
--- TKCIM/frmCHECKCOOKIESEDITD.cs
+++ TKCIM/frmCHECKCOOKIESEDITD.cs
@@ -45,22 +45,171 @@ namespace TKCIM
             ID = SUBID;
 
             SEARCHCHECKCOOKIESMD();
+            combobox3load();
+            combobox4load();
+
         }
 
         #region FUNCTION
-        public void SEARCHCHECKCOOKIESMD()
+        public void combobox3load()
         {
 
+            connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
+            sqlConn = new SqlConnection(connectionString);
+            String Sequel = "SELECT  [ID],[NAME] FROM [TKMOC].[dbo].[MANUEMPLOYEE] WHERE ID IN (SELECT ID FROM  [TKMOC].[dbo].[MANUEMPLOYEELIMIT]) ORDER BY ID";
+            SqlDataAdapter da = new SqlDataAdapter(Sequel, sqlConn);
+            DataTable dt = new DataTable();
+            sqlConn.Open();
+
+            dt.Columns.Add("ID", typeof(string));
+            dt.Columns.Add("NAME", typeof(string));
+            da.Fill(dt);
+            comboBox3.DataSource = dt.DefaultView;
+            comboBox3.ValueMember = "ID";
+            comboBox3.DisplayMember = "NAME";
+            sqlConn.Close();
+
+        }
+        public void combobox4load()
+        {
+
+            connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
+            sqlConn = new SqlConnection(connectionString);
+            String Sequel = "SELECT  [ID],[NAME] FROM [TKMOC].[dbo].[MANUEMPLOYEE] WHERE ID IN (SELECT ID FROM  [TKMOC].[dbo].[MANUEMPLOYEELIMIT]) ORDER BY ID";
+            SqlDataAdapter da = new SqlDataAdapter(Sequel, sqlConn);
+            DataTable dt = new DataTable();
+            sqlConn.Open();
+
+            dt.Columns.Add("ID", typeof(string));
+            dt.Columns.Add("NAME", typeof(string));
+            da.Fill(dt);
+            comboBox4.DataSource = dt.DefaultView;
+            comboBox4.ValueMember = "ID";
+            comboBox4.DisplayMember = "NAME";
+            sqlConn.Close();
+
+        }
+
+        public void SEARCHCHECKCOOKIESMD()
+        {
+            try
+            {
+                connectionString = ConfigurationManager.ConnectionStrings["dberp"].ConnectionString;
+                sqlConn = new SqlConnection(connectionString);
+
+                sbSql.Clear();
+                sbSqlQuery.Clear();
+
+
+                sbSql.AppendFormat(@"  SELECT  [MB002] AS '品名',CONVERT(varchar(100),[CHECKTIME],8) AS '時間',[WIGHT] AS '重量',[LENGTH] AS '長度',[TEMP] AS '溫度',[HUMIDITY] AS '溼度',[CHECKRESULT] AS '檢查結果',[OWNER] AS '填表人',[MANAGER]  AS '主管',[MAIN] AS '線別',[MAINDATE] AS '日期',[TARGETPROTA001] AS '單別',[TARGETPROTA002] AS '單號',[MB001] AS '品號',[ID]  ");
+                sbSql.AppendFormat(@"  FROM [TKCIM].dbo.[CHECKCOOKIESMD] WITH (NOLOCK)");
+                sbSql.AppendFormat(@" WHERE ID='{0}'", ID);
+                sbSql.AppendFormat(@"  ");
+
+
+                adapter = new SqlDataAdapter(@"" + sbSql, sqlConn);
+
+                sqlCmdBuilder = new SqlCommandBuilder(adapter);
+                sqlConn.Open();
+                ds1.Clear();
+                adapter.Fill(ds1, "TEMPds1");
+                sqlConn.Close();
+
+
+                if (ds1.Tables["TEMPds1"].Rows.Count == 0)
+                {
+
+                }
+                else
+                {
+                    if (ds1.Tables["TEMPds1"].Rows.Count >= 1)
+                    {
+                        SETVALUES();
+                    }
+                }
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                sqlConn.Close();
+            }
         }
 
         public void SETVALUES()
         {
+            textBox101.Text = ds1.Tables["TEMPds1"].Rows[0]["日期"].ToString();
+            textBox201.Text = ds1.Tables["TEMPds1"].Rows[0]["線別"].ToString();
+            textBox301.Text = ds1.Tables["TEMPds1"].Rows[0]["單別"].ToString();
+            textBox401.Text = ds1.Tables["TEMPds1"].Rows[0]["單號"].ToString();
+            textBox501.Text = ds1.Tables["TEMPds1"].Rows[0]["品名"].ToString();
+            textBox601.Text = ds1.Tables["TEMPds1"].Rows[0]["重量"].ToString();
+            textBox701.Text = ds1.Tables["TEMPds1"].Rows[0]["長度"].ToString();
+            textBox801.Text = ds1.Tables["TEMPds1"].Rows[0]["溫度"].ToString();
+            textBox901.Text = ds1.Tables["TEMPds1"].Rows[0]["溼度"].ToString();
 
+            comboBox1.Text = ds1.Tables["TEMPds1"].Rows[0]["檢查結果"].ToString();
+            comboBox3.Text = ds1.Tables["TEMPds1"].Rows[0]["填表人"].ToString();
+            comboBox4.Text = ds1.Tables["TEMPds1"].Rows[0]["主管"].ToString();
+
+            dateTimePicker3.Value = Convert.ToDateTime(ds1.Tables["TEMPds1"].Rows[0]["時間"].ToString());
         }
 
         public void UPDATECHECKCOOKIESMD()
         {
+            try
+            {
+                connectionString = ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString;
+                sqlConn = new SqlConnection(connectionString);
 
+                sqlConn.Close();
+                sqlConn.Open();
+                tran = sqlConn.BeginTransaction();
+
+                sbSql.Clear();
+                if (!string.IsNullOrEmpty(textBox701.Text) && !string.IsNullOrEmpty(textBox801.Text))
+                {
+                    sbSql.AppendFormat(" UPDATE [TKCIM].dbo.[CHECKCOOKIESMD]");
+                    sbSql.AppendFormat(" SET [WIGHT]='{0}',[LENGTH]='{1}',[TEMP]='{2}',[HUMIDITY]='{3}'",textBox601.Text, textBox701.Text, textBox801.Text, textBox901.Text);
+                    sbSql.AppendFormat(" WHERE ID='{0}'", ID);
+                    sbSql.AppendFormat(" ");
+                    sbSql.AppendFormat(" ");
+                }
+
+
+
+                sbSql.AppendFormat(" ");
+
+                cmd.Connection = sqlConn;
+                cmd.CommandTimeout = 60;
+                cmd.CommandText = sbSql.ToString();
+                cmd.Transaction = tran;
+                result = cmd.ExecuteNonQuery();
+
+                if (result == 0)
+                {
+                    tran.Rollback();    //交易取消
+                }
+                else
+                {
+                    tran.Commit();      //執行交易  
+
+
+                }
+
+            }
+            catch
+            {
+
+            }
+
+            finally
+            {
+                sqlConn.Close();
+            }
         }
 
         #endregion
@@ -68,7 +217,9 @@ namespace TKCIM
         #region BUTTON
         private void button8_Click(object sender, EventArgs e)
         {
+            UPDATECHECKCOOKIESMD();
 
+            this.Close();
         }
         #endregion
 
